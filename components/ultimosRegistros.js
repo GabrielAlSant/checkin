7091c34647bee8b5a53b57d5c88ee5bd66e81e17
gabriel <gabrielalvesdossantos16@gmail.com>
2025-07-09 15:40:31 -0400

    estilização home pronta

---
+++ components/ultimosRegistros.js
@@ -0,0 +1,55 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { styles } from "../pages/Home";
+
+
+export default function UltimosRegistros({historico}){
+    return(
+        <View>
+            {historico.length > 0 && (
+                    <View style={{ marginBottom: 20 }}>
+                      <Text style={styles.ultRegistros}>Últimos registros</Text>
+                      <FlatList
+                        data={historico}
+                        keyExtractor={(item) => item.id.toString()}
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        renderItem={({ item }) => {
+                          const data = new Date(item.data_hora_checkin);
+                          const hora = data.toLocaleTimeString([], {
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          });
+            
+                          return (
+                            <TouchableOpacity
+                              onPress={() =>
+                                navigation.navigate("DetalhesCheckin", { id: item.id })
+                              }
+                            >
+                              <View style={styles.historicoItem}>
+                                <Text style={styles.historicoHora}>{hora}</Text>
+                                <Text style={styles.historicoNome}>
+                                  {item.apelido || item.nome}
+                                </Text>
+                                {item.id % 2 === 1 ? (
+                                  <Text style={styles.historicoIn}>Check-in</Text>
+                                ) : (
+                                  <Text style={styles.historicoOut}>Check-out</Text>
+                                )}
+                              </View>
+                            </TouchableOpacity>
+                          );
+                        }}
+                      />
+                    </View>
+                  )}
+        </View>
+    )
+}
--- pages/Home.js
+++ pages/Home.js
@@ -1,15 +1,31 @@
-import React, { useState, useCallback } from 'react';
-import { StyleSheet, Text, View, FlatList, Button,TouchableOpacity  } from 'react-native';
-import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import React, { useState, useCallback } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import {
   createTables,
   getAtividadesPorDia,
   verificarStatusAtividade,
-  getHistoricoRecentes
-} from '../lib/database';
+  getHistoricoRecentes,
+} from "../lib/database";
+import UltimosRegistros from "../components/ultimosRegistros";
 
 export default function Home() {
-  const dias = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+  const dias = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
   const hoje = new Date();
   const diaSemana = dias[hoje.getDay()];
 
@@ -42,36 +58,9 @@ export default function Home() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hoje é {diaSemana}</Text>
+      <UltimosRegistros historico={historico}/>
 
-      {historico.length > 0 && (
-        <View style={{ marginBottom: 20 }}>
-          <Text style={styles.subTitle}>Últimos registros</Text>
-          <FlatList
-            data={historico}
-            keyExtractor={(item) => item.id.toString()}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            renderItem={({ item }) => {
-              const data = new Date(item.data_hora_checkin);
-              const hora = data.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-
-              return (
-              <TouchableOpacity onPress={() => navigation.navigate('DetalhesCheckin', { id: item.id })}>
-  <View style={styles.historicoItem}>
-    <Text style={styles.historicoHora}>{hora}</Text>
-    <Text style={styles.historicoNome}>{item.apelido || item.nome}</Text>
-    <Text style={styles.historicoTipo}>
-      {(item.id % 2 === 1) ? 'Check-in' : 'Check-out'}
-    </Text>
-  </View>
-</TouchableOpacity>
-
-              );
-            }}
-          />
-        </View>
-      )}
+    <Text style={styles.title}>Atividades para {diaSemana}</Text>
 
       {atividades.length === 0 ? (
         <Text style={styles.semAtividades}>Nenhuma atividade para hoje</Text>
@@ -80,16 +69,30 @@ export default function Home() {
           data={atividades}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => {
-            const status = statusAtividades[item.id] || 'checkin';
+            const status = statusAtividades[item.id] || "checkin";
             return (
               <View style={styles.itemContainer}>
                 <View style={{ flex: 1 }}>
-                  <Text style={styles.itemText}>{item.apelido || item.nome}</Text>
+                  <Text style={styles.itemText}>
+                    {item.apelido || item.nome}
+                  </Text>
                 </View>
-                <Button
-                  title={`Fazer ${status}`}
-                  onPress={() => navigation.navigate('EfetuarCheckin', { id: item.id })}
-                />
+             {status == 'checkin' ? <Button
+                  color="green"
+                  title={status}
+                  style={{ padding: 10 }}
+                  onPress={() =>
+                    navigation.navigate("EfetuarCheckin", { id: item.id })
+                  }
+                />:
+                   <Button
+                  color="red"
+                  title={status}
+                  style={{ padding: 10 }}
+                  onPress={() =>
+                    navigation.navigate("EfetuarCheckin", { id: item.id })
+                  }
+                />}
               </View>
             );
           }}
@@ -99,42 +102,62 @@ export default function Home() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, backgroundColor: '#fff' },
-  title: { fontSize: 22, fontWeight: 'bold', marginBottom: 16 },
-  subTitle: { fontSize: 16, fontWeight: 'bold', marginBottom: 8 },
-  semAtividades: { fontSize: 16, color: 'gray' },
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#121212",
+    color: "green",
+  },
+  title: { fontSize: 22, fontWeight: "bold", marginBottom: 16, color: "white" },
+  ultRegistros: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 8,
+    color: "white",
+  },
+  semAtividades: { fontSize: 16, color: "gray" },
   itemContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 10,   
+    borderTopWidth: 1,
     borderBottomWidth: 1,
-    borderColor: '#ccc'
+    backgroundColor: "#282828",
   },
   itemText: {
     fontSize: 16,
-    fontWeight: '500'
+    fontWeight: "500",
+    color: "white",
+    marginLeft:10
   },
   historicoItem: {
-    backgroundColor: '#f1f1f1',
+    backgroundColor: "#f1f1f1",
     padding: 12,
     borderRadius: 8,
     marginRight: 10,
-    alignItems: 'center',
-    width: 120
+    alignItems: "center",
+    width: 120,
   },
   historicoHora: {
     fontSize: 16,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   historicoNome: {
     fontSize: 14,
-    marginTop: 4
+    marginTop: 4,
   },
-  historicoTipo: {
+  historicoIn: {
     fontSize: 13,
-    color: '#555',
-    marginTop: 2
+    color: "#555",
+    marginTop: 2,
+    color:"green"
+  },
+   historicoOut: {
+    fontSize: 13,
+    color: "#555",
+    marginTop: 2,
+    color:"red"
   }
 });
